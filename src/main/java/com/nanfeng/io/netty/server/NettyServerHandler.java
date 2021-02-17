5bbc63508e7949ce4500deff0adaf69ac82cbd6d
LHJ <lhj8023@outlook.com>
2021-02-17 18:02:24 +0800

    [modify][添加Netty异步任务]

--- src/main/java/com/nanfeng/io/netty/server/NettyServerHandler.java
+++ src/main/java/com/nanfeng/io/netty/server/NettyServerHandler.java
@@ -7,6 +7,7 @@ import io.netty.channel.ChannelInboundHandlerAdapter;
 import lombok.extern.slf4j.Slf4j;
 
 import java.nio.charset.StandardCharsets;
+import java.util.concurrent.TimeUnit;
 
 /**
  * @author nanfeng
@@ -18,6 +19,36 @@ public class NettyServerHandler extends ChannelInboundHandlerAdapter {
     public void channelRead(ChannelHandlerContext ctx, Object msg) throws Exception {
         ByteBuf buf = (ByteBuf) msg;
         log.info("[NettyServerHandler] Receive Msg From Client, msg => {}", buf.toString(StandardCharsets.UTF_8));
+
+        // 模拟耗时操作, 异步解决
+        ctx.channel().eventLoop().execute(() -> {
+            try {
+                Thread.sleep(5000);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+
+            String content = "Netty Server Execute Task-1, Thread: " + Thread.currentThread().getName();
+            ctx.writeAndFlush(Unpooled.copiedBuffer(content.getBytes(StandardCharsets.UTF_8)));
+        });
+
+        ctx.channel().eventLoop().execute(() -> {
+            try {
+                Thread.sleep(5000);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+
+            String content = "Netty Server Execute Task-2, Thread: " + Thread.currentThread().getName();
+            ctx.writeAndFlush(Unpooled.copiedBuffer(content.getBytes(StandardCharsets.UTF_8)));
+        });
+
+        // 定时任务
+        ctx.channel().eventLoop().schedule(() -> {
+            String content = "This Is Schedule Task, Thread: " + Thread.currentThread().getName();
+            ctx.writeAndFlush(Unpooled.copiedBuffer(content.getBytes(StandardCharsets.UTF_8)));
+        }, 5, TimeUnit.SECONDS);
+
     }
 
     @Override
